8a928742d34136f2542cb55773ddc23a4067e0c7
JavaCommons Technologies <javacommons@gmail.com>
2025-05-27 08:25:36 +0900

    cs_scan: Initial release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,12 @@
 # CHANGELOG.md
+
+## 2025.527.825
+
+- Initial release
+
+```
+--- a/pubspec.yaml
++++ b/pubspec.yaml
+-version: 0.0.1
++version: 2025.527.825
+```
--- test/run_test.dart
+++ test/run_test.dart
@@ -3,12 +3,22 @@ import 'package:test/test.dart';
 import 'package:debug_output/debug_output.dart';
 import 'package:cs_scan/cs_scan.dart';
 
-main() {
+void main() {
   group('Run', () {
     test('run1', () {
-      var calc = Calculator();
-      var result = calc.addOne(123);
-      dump(result, title: 'result');
+      String result;
+      var csScan = CsScan('~/cs-cmd/Test.Main/Test.Main.cs');
+      result = echo(csScan.$sourceSet);
+      expect(
+        result,
+        equals(
+          '''{Test.Main.cs, Test.Main.api.cs, lib/lib.cs, lib/lib.Add3.cs}''',
+        ),
+      );
+      result = echo(csScan.$refSet);
+      expect(result, equals('''{Test.Lib.exe}'''));
+      result = echo(csScan.$embedSet);
+      expect(result, equals('''{test.txt}'''));
     });
   });
 }
