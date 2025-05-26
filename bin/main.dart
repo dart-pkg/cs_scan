dd515ed8d00f59dee72cff5a5b4815eca3e4d58c
JavaCommons Technologies <javacommons@gmail.com>
2025-05-26 22:21:44 +0900

    .

--- bin/main.dart
+++ bin/main.dart
@@ -2,13 +2,17 @@
 
 import 'package:cs_scan/cs_scan.dart';
 import 'package:debug_output/debug_output.dart';
-import 'package:std/std.dart' as std_std;
+import 'package:std/misc.dart';
+//import 'package:std/std.dart' as std_std;
 
 void main() {
   dump(add2(11, 22));
-  String mainSrc = std_std.pathExpand('~/cs-cmd/Test.Main/Test.Main.cs');
+  String cwd = getCwd();
+  String mainSrc = pathExpand('~/cs-cmd/Test.Main/Test.Main.cs');
+  String mainDir = pathDirectoryName(mainSrc);
+  setCwd(mainDir);
   echo(mainSrc);
-  List<String> lines = std_std.readFileLines(mainSrc);
+  List<String> lines = readFileLines(mainSrc);
   lines = lines.where((x) => x.startsWith('//')).toList();
   echo(lines, type: 'json');
   final reg = RegExp(r'//css_inc[ ]+(.+)[ ]*');
@@ -17,6 +21,10 @@ void main() {
     RegExpMatch? match = reg.firstMatch(line);
     if (match != null) {
       echo(match.group(1));
+      String src = pathExpand(match.group(1)!);
+      String rel = pathRelative(src, from: mainDir);
+      echo(rel, title: r'rel');
     }
   }
+  setCwd(cwd);
 }
