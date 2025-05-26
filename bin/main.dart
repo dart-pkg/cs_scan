93525201abe0de3c14fc06f78f6dc08e6de09fcb
JavaCommons Technologies <javacommons@gmail.com>
2025-05-27 06:15:30 +0900

    .

--- bin/main.dart
+++ bin/main.dart
@@ -8,27 +8,36 @@ import 'package:std/misc.dart';
 Set<String> $sourceSet = {};
 List<String> $sourceList = [];
 Set<String> $refSet = {};
+Set<String> $embedSet = {};
 
-void scanSource(String path) {
+void scanSource(String path, String fromDir) {
   path = pathExpand(path);
-  $sourceSet.add(path);
+  $sourceSet.add(pathRelative(path, from: fromDir));
   String dir = pathDirectoryName(path);
   pushd(dir);
   List<String> lines = readFileLines(path);
   final regInc = RegExp(r'^//css_inc[ ]+(.+)[ ]*');
   final regRef = RegExp(r'^//css_ref[ ]+(.+)[ ]*');
+  final regEmbed = RegExp(r'^//css_embed[ ]+(.+)[ ]*');
   for (int i = 0; i < lines.length; i++) {
     String line = lines[i];
     RegExpMatch? matchInc = regInc.firstMatch(line);
     if (matchInc != null) {
       String src = pathExpand(matchInc.group(1)!);
-      $sourceSet.add(src);
-      scanSource(src);
+      String rel = pathRelative(src, from: fromDir);
+      $sourceSet.add(rel);
+      scanSource(src, fromDir);
     }
     RegExpMatch? matchRef = regRef.firstMatch(line);
     if (matchRef != null) {
       String src = matchRef.group(1)!;
       $refSet.add(src);
+    }
+    RegExpMatch? matchEmbed = regEmbed.firstMatch(line);
+    if (matchEmbed != null) {
+      String src = pathExpand(matchEmbed.group(1)!);
+      String rel = pathRelative(src, from: fromDir);
+      $embedSet.add(rel);
     }
   }
   popd();
@@ -39,12 +48,14 @@ void main() {
   String cwd = getCwd();
   String mainSrc = pathExpand('~/cs-cmd/Test.Main/Test.Main.cs');
   String mainDir = pathDirectoryName(mainSrc);
-  scanSource(mainSrc);
-  $sourceList = $sourceSet.toList();
-  echo($sourceList, type: 'json');
-  for (int i=0; i<$sourceList.length; i++) {
-    String rel = pathRelative($sourceList[i], from: mainDir);
-    echo(rel, title: r'rel');
-  }
+  scanSource(mainSrc, mainDir);
+  echo($sourceSet);
+  //$sourceList = $sourceSet.toList();
+  //echo($sourceList, type: 'json');
+  // for (int i=0; i<$sourceList.length; i++) {
+  //   String rel = pathRelative($sourceList[i], from: mainDir);
+  //   echo(rel, title: r'rel');
+  // }
   echo($refSet);
+  echo($embedSet);
 }
